c990aa7e42a71ec058f2ce20c7486f7769916766
LordBaryhobal <lordbaryhobal@gmail.com>
2025-10-21 14:33:58 +0200

    feat: add move API endpoint

--- src/server.py
+++ src/server.py
@@ -1,9 +1,32 @@
+from chess import Board, IllegalMoveError
 from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI()
 
 router = APIRouter()
+
+
+class MoveCheckData(BaseModel):
+    fen: str
+    move: str
+
+
+@router.post("/move/")
+def apply_move(move: MoveCheckData):
+    board: Board = Board(move.fen)
+    try:
+        board.push_uci(move.move)
+        return {
+            "valid": True,
+            "fen": board.fen()
+        }
+    except IllegalMoveError:
+        return {
+            "valid": False
+        }
+
 
 app.include_router(router, prefix="/api")
 app.mount("/", StaticFiles(
